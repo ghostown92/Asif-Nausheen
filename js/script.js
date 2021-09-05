d5cd7c1e93d9bed755dc97a19009f78ee8f398df
Mohd Furqan <79809897+ghostown92@users.noreply.github.com>
2021-09-05 21:06:15 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@
 
 
 // Set the date we're counting down to
-var countDownDate = new Date("October 01, 2021 19:00:00").getTime();
+var countDownDate = new Date("October 01, 2020 19:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
